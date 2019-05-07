9658f8ff2ab53e9f731c9d541313a7606c8a7c12
jason <jiexinzhang@sina.cn>
2019-05-07 17:05:59 +0800

    排序方法
    木桶排序，选择排序

--- Sort.py
+++ Sort.py
@@ -88,8 +88,90 @@ print("排序结果如下:")
 print(num2,count,count_swap)
 
 
-# In[ ]:
+# In[56]:
 
 
+'''
+木桶排序（）
+'''
+# 定义列表
+num_list=[6,9,8,7,2,5,1]
+# 测试列表
+print(num_list)
 
+# 列表的长度
+length = len(num_list)
+print(length) # 测试列表长度
+
+# 定义空桶,因为元素中有9，所以需要至少10个桶
+Bucket = [0,0,0,0,0,0,0,0,0,0]
+#Bucket[1] = 1
+print(Bucket)
+
+for i in range(length):        
+#     用列表num_list的值用作，“木桶”的下标
+    value = num_list[i]
+#     将对应的值给对应的“桶”，3在3号桶
+    Bucket[value] =num_list[i]
+
+# 打印排序后的序列，没有去处零
+print(Bucket)
+
+# 打印排序后的列表
+length_Bucket = len(Bucket)
+for j in range(length_Bucket):
+    if Bucket[j] == 0:
+        continue
+
+    print(Bucket[j],end=",  ")
+
+  
+    
+# 未解决问题：如果给定的列表中有0怎样处理会更简便？
+    
+
+
+# In[17]:
+
+
+'''
+选择排序
+'''
+
+# 思想：找到极值（极大值、极小值）的然后跟换下标，每一趟全部比较完后，再将元素放在开头或者结尾
+
+# 定义一个列表
+m_list =[[1,9,8,5,6,7,4,3,2],[1,2,3,4,5,6,7,8,9]]
+
+# 把列表的值取出来
+nums = m_list[0]
+# 求列表的长度
+length = len(nums)
+
+print(nums)
+print(length)
+
+# 由于该算法主要耗时间的地方就是比较和交换，做标记变量
+count_swap = 0    #交换
+count_iter = 0   #比较
+
+
+# 列表值比较
+for i in range(length):
+#     定义最大数字的索引
+    maxindex = i
+    for j in range(i+1,length):   #for j in range(1, length-i) 与前面语句执行结果一致
+        count_iter += 1  #比较计数
+#         最大数字与下标为j数字进行比较
+        if nums[maxindex] < nums[j]:
+               maxindex = j
+#     减少交换次数，如果i=maxindex 不用进行交换        
+    if i != maxindex:
+        tmp = nums[i]
+        nums[i] = nums[maxindex]
+        nums[maxindex] = tmp
+        count_swap += 1   #交换计数
+        
+print(nums,count_iter,count_swap)
+        
 
